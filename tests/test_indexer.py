3684737f1028d9cc25784ec1164195b515018146
SHIRUIZHAOIILLiil <sc23sz@leeds.ac.uk>
2026-05-02 00:09:52 +0100

    test: expand unit coverage for indexing and search

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -8,6 +8,15 @@ class IndexerTests(unittest.TestCase):
     def test_tokenize_is_case_insensitive(self):
         self.assertEqual(tokenize("Good, GOOD friend's"), ["good", "good", "friend's"])
 
+    def test_tokenize_handles_empty_text(self):
+        self.assertEqual(tokenize(""), [])
+
+    def test_tokenize_ignores_punctuation(self):
+        self.assertEqual(tokenize("Hello... world!"), ["hello", "world"])
+
+    def test_tokenize_keeps_numbers(self):
+        self.assertEqual(tokenize("Page 10 has 2 quotes"), ["page", "10", "has", "2", "quotes"])
+
     def test_build_index_records_frequency_and_positions(self):
         pages = [CrawledPage(url="page-1", text="Good friends are good")]
 
@@ -17,6 +26,25 @@ class IndexerTests(unittest.TestCase):
         self.assertEqual(index["good"]["page-1"]["positions"], [0, 3])
         self.assertEqual(index["friends"]["page-1"]["positions"], [1])
 
+    def test_build_index_keeps_pages_separate(self):
+        pages = [
+            CrawledPage(url="page-1", text="good good"),
+            CrawledPage(url="page-2", text="good"),
+        ]
+
+        index = build_index(pages)
+
+        self.assertEqual(index["good"]["page-1"]["frequency"], 2)
+        self.assertEqual(index["good"]["page-1"]["positions"], [0, 1])
+        self.assertEqual(index["good"]["page-2"]["frequency"], 1)
+        self.assertEqual(index["good"]["page-2"]["positions"], [0])
+
+    def test_build_index_handles_empty_page_text(self):
+        pages = [CrawledPage(url="page-1", text="")]
+
+        index = build_index(pages)
+
+        self.assertEqual(index, {})
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_search.py
+++ tests/test_search.py
@@ -18,12 +18,36 @@ class SearchTests(unittest.TestCase):
     def test_print_word_returns_posting_case_insensitively(self):
         self.assertEqual(print_word(self.index, "GOOD"), self.index["good"])
 
+    def test_print_word_returns_empty_dict_for_missing_word(self):
+        self.assertEqual(print_word(self.index, "missing"), {})
+
+    def test_print_word_returns_empty_dict_for_punctuation_only_input(self):
+        self.assertEqual(print_word(self.index, "!!!"), {})
+
+    def test_print_word_uses_first_token_only(self):
+        self.assertEqual(print_word(self.index, "good friends"), self.index["good"])
+
     def test_find_pages_returns_pages_containing_all_words(self):
         self.assertEqual(find_pages(self.index, "good friends"), ["page-2"])
+
+    def test_find_pages_returns_single_word_matches(self):
+        self.assertEqual(find_pages(self.index, "good"), ["page-1", "page-2"])
+
+    def test_find_pages_is_case_insensitive(self):
+        self.assertEqual(find_pages(self.index, "GOOD FRIENDS"), ["page-2"])
+
+    def test_find_pages_ignores_query_punctuation(self):
+        self.assertEqual(find_pages(self.index, "good, friends!"), ["page-2"])
+
+    def test_find_pages_handles_repeated_query_terms(self):
+        self.assertEqual(find_pages(self.index, "good good"), ["page-1", "page-2"])
 
     def test_find_pages_handles_empty_and_missing_queries(self):
         self.assertEqual(find_pages(self.index, ""), [])
         self.assertEqual(find_pages(self.index, "missing"), [])
+
+    def test_find_pages_returns_empty_list_when_one_query_term_is_missing(self):
+        self.assertEqual(find_pages(self.index, "good missing"), [])
 
 
 if __name__ == "__main__":
